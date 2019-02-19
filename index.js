3b839e2558847ebbba7bbd645b88e6c7eacc7c4f
Zaymon Foulds-Cook <generation281@gmail.com>
2019-02-19 19:07:52 +1000

    Change indexOf to includes for clarity

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const getCollectionFileNames = () => {
 const parseFileNames = (files) => {
   if (!files) { throw 'You don\'t have any collection markdown files in ./collections' }
 
-  const markdownFiles = files.filter(f => f.indexOf('.md') >= 0)
+  const markdownFiles = files.filter(f => f.includes('.md'))
   const trimmedFileNames = markdownFiles.map(f => f.replace('.md', ''))
   return trimmedFileNames
 }
